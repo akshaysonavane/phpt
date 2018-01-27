69b4323540d3a08511a70e5dacdb2f81eb57b5b0
akshay <akshaysonavane0@gmail.com>
2018-01-27 11:00:49 +0530

    Hellow world
    php coding

---
+++ 01Hellow.php
@@ -0,0 +1,25 @@
+<html>
+   
+   <head>
+      <title>Hello World</title>
+   </head>
+   
+   <body>
+      
+
+
+
+      <!-- <?php echo "Hello, World!";?> -->
+
+
+
+<!-- <?php 
+echo "Hello, World!";?> 
+ -->
+
+
+<script language = "php">  echo "asda asd asdsd asdasda " </script>
+
+   </body>
+
+</html>
